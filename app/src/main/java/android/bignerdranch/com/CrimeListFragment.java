34e001231d46adb72cbc1dcaf85f5856e4f4a157
Nizar <nizar4790k@gmail.com>
2019-07-07 17:40:55 -0400

    Added formated date

--- app/src/main/java/android/bignerdranch/com/CrimeListFragment.java
+++ app/src/main/java/android/bignerdranch/com/CrimeListFragment.java
@@ -1,14 +1,17 @@
 package android.bignerdranch.com;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -43,6 +46,8 @@ public class CrimeListFragment  extends Fragment {
 
         private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
+
+            private ImageView  mSolvedImageView;
             private TextView mTitleTextView;
             private TextView mDateTextView;
             private Crime mCrime;
@@ -53,12 +58,14 @@ public class CrimeListFragment  extends Fragment {
                     mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
                     mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
                     itemView.setOnClickListener(this);
+                    mSolvedImageView =(ImageView) itemView.findViewById(R.id.crime_solved);
                 }
 
                 public void bind(Crime crime) {
                 mCrime = crime;
                 mTitleTextView.setText(mCrime.getTitle());
-                mDateTextView.setText(mCrime.getDate().toString());
+                mDateTextView.setText(DateFormat.format("mm-dd-yyyy",mCrime.getDate()));
+                mSolvedImageView.setVisibility(crime.isSolved() ? View.VISIBLE: View.GONE);
 
                 }
 
